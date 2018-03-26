1b88cfe8a2d522bf3da76b171c44a12237c7e2f0
Yofti <yofti@me.com>
2018-03-26 14:20:31 -0700

    new install and uninstall scripts

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-LOG_FILE=logs/install/install-$(date "+%Y.%m.%d-%H.%M.%S").log
+# Global Variables
+NOW=$(date "+%Y.%m.%d-%H.%M.%S")
+SDC_HOME=".."
+LOG_FILE=$SDC_HOME/logs/install/install-$NOW.log
+SETTINGS_FILE="$SDC_HOME/etc/config/sdc-settings.yaml"
+NAMESPACE=$(egrep sysdigNamespace $SETTINGS_FILE |awk '{print $2}')
+BACKEND_VERSION=$(egrep sysdigBackendImage $SETTINGS_FILE | awk -F: '{print $3}')
+FRONTEND_VERSION=$(egrep sysdigFrontendImage $SETTINGS_FILE | awk -F: '{print $3}')
+K8S_CLIENT_VERSION=$(kubectl version | egrep ^Client | awk -F, '{print $3}')
+K8S_SERVER_VERSION=$(kubectl version | egrep ^Server | awk -F, '{print $3}')
+CURRENT_CONTEXT=$(kubectl config get-contexts|egrep '^\*'|awk '{print $2}')
+CURRENT_CLUSTER=$(kubectl config get-contexts|egrep '^\*'|awk '{print $3}')
 
-CONFIG_FILE="etc/sdc-config.yaml"
-CLOUD_PROVIDER="GKE"
-BACKEND_VERSION="800"
-FRONTEND_VERSION="0.78.1"
 
 error_exit()
 {
@@ -14,169 +21,190 @@ error_exit()
 }
 
 
-echo > $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a $LOG_FILE
-echo | tee -a $LOG_FILE
-echo "This is the Sysdig Monitor on-prem Kubernetes installer." 					 | tee -a $LOG_FILE
-echo "This installer assumes you have a running kubernetes cluster on AWS or GKE."   | tee -a $LOG_FILE 
-echo "The executable 'kubectl' needs to be in your \$PATH."       					 | tee -a $LOG_FILE 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a $LOG_FILE
+print_env_variables()
+{
+	printf "LOG_FILE: %s\n" $LOG_FILE
+	printf "SDC_HOME: %s\n" $SDC_HOME
+	printf "SETTINGS_FILE: %s\n" $SETTINGS_FILE
+	printf "NAMESPACE: %s\n" $NAMESPACE
+	printf "BACKEND_VERSION: %s\n" $BACKEND_VERSION
+	printf "FRONTEND_VERSION: %s\n" $FRONTEND_VERSION
+	printf "K8S_CLIENT_VERSION: %s\n" $K8S_CLIENT_VERSION
+	printf "K8S_SERVER_VERSION: %s\n" $K8S_SERVER_VERSION
+	printf "CURRENT_CONTEXT: %s\n" $CURRENT_CONTEXT
+	printf "CURRENT_CLUSTER: %s\n" $CURRENT_CLUSTER
 
-echo | tee -a $LOG_FILE
-echo | tee -a $LOG_FILE
+}
 
-echo "Your current kubectl client and server version are as follows:" | tee -a $LOG_FILE				
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"| tee -a $LOG_FILE 
-K8S_CLIENT_VERSION=`kubectl version | egrep ^Client | awk -F, '{print $3}'`
-K8S_SERVER_VERSION=`kubectl version | egrep ^Server | awk -F, '{print $3}'`
-echo "Client: $K8S_CLIENT_VERSION" | tee -a $LOG_FILE
-echo "Server: $K8S_SERVER_VERSION"| tee -a $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a $LOG_FILE
-echo
-echo
+print_banner()
+{
+	local cols=$(tput cols)
+	local lines=$(tput lines)
+	local numcols=$(((cols-6)/2))
+	local numlines=$((lines/2))
 
-echo "Your current Kubernetes context is:"| tee -a $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"| tee -a $LOG_FILE
-CURRENT_CONTEXT=`kubectl config get-contexts|egrep '^\*'|awk '{print $2}'`
-CURRENT_CLUSTER=`kubectl config get-contexts|egrep '^\*'|awk '{print $3}'`
-echo "Current Context: $CURRENT_CONTEXT"| tee -a $LOG_FILE
-echo "Current Cluster: $CURRENT_CLUSTER"| tee -a $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo| tee -a $LOG_FILE
-echo| tee -a $LOG_FILE
+	clear
+	printf '\n'
+	printf '+%.0s' {1..100}
+	printf '\n'
+	#tput cup $numlines $numcols
+	printf "%s\n" "sysdigcloud on-prem installer"
+	printf '\n%.0s' {1..3}
+	printf "%s\n" "This is the Sysdig Monitor on-prem Kubernetes installer."
+	printf "%s\n" "This installer assumes you have a running kubernetes cluster on AWS or GKE."
+	printf "%s\n" "The executable 'kubectl' needs to be in your \$PATH with the context pointing to the right cluster."
+	printf '\n%.0s' {1..3}
+	printf "%s\n" "Your current kubectl client and server version are as follows:"
+	printf "Client: %s\n" $K8S_CLIENT_VERSION
+	printf "Server: %s\n" $K8S_SERVER_VERSION
+	printf '\n%.0s' {1..2}
+	printf "%s\n" "Your current Kubernetes context is:"
+	printf "Current Context: %s\n" $CURRENT_CONTEXT
+	printf "Current Cluster: %s\n" $CURRENT_CLUSTER
+	printf '+%.0s' {1..100}
+	printf '\n'
+}
 
-echo "Your current Kubernetes cluster on $CLOUD_PROVIDER is:"| tee -a $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"| tee -a $LOG_FILE
-kubectl cluster-info| tee -a $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"| tee -a $LOG_FILE
-echo 
+prompt_install()
+{
+	printf '\n'
+	printf '\n'
+	printf "%s\n" "Do you wish to install backend version $BACKEND_VERSION of sdc-kubernetes?"| tee -a $LOG_FILE
+	select yn in "Yes" "No"; do
+	    case $yn in
+	        Yes ) break;;
+	        No ) exit 1;;
+	    esac
+	done
+	printf '\n'
 
-echo "Do you wish to install backend version $BACKEND_VERSION of sdc-kubernetes along with agents with version $FRONTEND_VERSION?"| tee -a $LOG_FILE
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit 1;;
-    esac
-done
+}
 
-# create namespace if it doesn't exist
-kubectl get namespace sysdigcloud >> $LOG_FILE 2>&1
-if [ $? -ne 0 ]	; then
-	kubectl create namespace sysdigcloud >> $LOG_FILE 2>&1 
-	if [ $? -eq 0 ] ; then
-		echo "... namespace sysdigcloud created."| tee -a $LOG_FILE
-	else
-		echo "... failed to create namespace sysdigcloud."| tee -a $LOG_FILE
-		exit 1
+
+
+
+create_namespace()
+{
+	# create namespace if it doesn't exist
+	kubectl get namespace $NAMESPACE >> $LOG_FILE 2>&1
+	if [ $? -ne 0 ]	; then
+		kubectl create namespace $NAMESPACE >> $LOG_FILE 2>&1 
+		if [ $? -eq 0 ] ; then
+			echo "... namespace $NAMESPACE created."| tee -a $LOG_FILE
+		else
+			echo "... failed to create namespace $NAMESPACE."| tee -a $LOG_FILE
+			exit 1
+		fi
+	else 
+		echo "... namespace $NAMESPACE already exists."| tee -a $LOG_FILE
 	fi
-else 
-	echo "... namespace sysdigcloud already exists."| tee -a $LOG_FILE
-fi
-
-# create storageclasses
-kubectl create -f datastores/storageclasses/ >> $LOG_FILE 2>&1
-#if [ $? -ne 0 ]; then
-#	echo "...failed to create storageclasses."
-#	exit 1
-#fi
-
-# creating ssl certs
-openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj \
-"/C=US/ST=CA/L=SanFrancisco/O=ICT/CN=sysdig.yoftilabs.com" \
--keyout etc/certs/server.key -out etc/certs/server.crt >> $LOG_FILE 2>&1
-
-if [ $? -ne 0 ]; then
-	echo "... failed to create ssl certs."| tee -a $LOG_FILE
-	exit 1
-else
-	echo "... ssl certs created."| tee -a $LOG_FILE
-fi
+}
 
 
-#create ssl-secret in kubernetes if it doesn't exist already
-kubectl get secret sysdigcloud-ssl-secret --namespace sysdigcloud>> $LOG_FILE 2>&1
-if [ $? -ne 0 ]	; then
-	kubectl create secret tls sysdigcloud-ssl-secret --cert=etc/certs/server.crt --key=etc/certs/server.key --namespace=sysdigcloud >> $LOG_FILE 2>&1
+create_storageclasses()
+{
+	kubectl create -f $SDC_HOME/datastores/storageclasses/ >> $LOG_FILE 2>&1
 	if [ $? -ne 0 ]; then
-		echo "... failed to create ssl secret in kubernetes."| tee -a $LOG_FILE
-		exit 1
+		echo "... failed to create storageclasses."| tee -a $LOG_FILE
+		echo "     ... continuing."
 	else
-		echo "... ssl secret created in kubernetes. "| tee -a $LOG_FILE
-	fi
-else
-	echo "... ssl secret sysdigcloud-ssl-secret already exists."| tee -a $LOG_FILE
-fi
+		echo "... storageclasses created."| tee -a $LOG_FILE
+	fi	
+}
 
-if [ -e $QUAY_FILE ]; then
-	quay_key=`cat $QUAY_FILE`
-else
-	echo "... failed to find $QUAY_FILE" | tee -a $LOG_FILE
-	echo "... make sure you have the sysdig provided quay secret file under sdc-kubernetes/$QUAY_FILE" | tee -a $LOG_FILE
-	exit 1
-fi
+create_ssl_certs()
+{
+	openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj \
+	"/C=US/ST=CA/L=SanFrancisco/O=ICT/CN=sysdig.yoftilabs.com" \
+	-keyout $SDC_HOME/etc/certs/server.key -out $SDC_HOME/etc/certs/server.crt >> $LOG_FILE 2>&1
 
-if [ -e $LICENSE_FILE ]; then
-	license_key=`cat $LICENSE_FILE`
-else
-	echo "... failed to find $LICENSE_FILE" | tee -a $LOG_FILE
-	echo "... make sure you have the sysdig provided licene file under sdc-kubernetes/$LICENSE_FILE" | tee -a $LOG_FILE
-	exit 1
-fi
-
-#insert quay and license keys into the Configmap file
-#hack to avoid posting license and quay key to github
-sed -i .bak "s/.dockerconfigjson: \"\"/.dockerconfigjson: \"${quay_key}\"/" $CONFIG_FILE
-sed -i .bak "s/sysdigcloud.license: \"\"/sysdigcloud.license: \"${license_key}\"/" $CONFIG_FILE
-
-kubectl get configmap sysdigcloud-config >> $LOG_FILE 2>&1
-if [ $? -ne 0 ]	; then
-	kubectl create -f $CONFIG_FILE || error_exit "... unable to create configmap. Exiting." | tee -a $LOG_FILE
 	if [ $? -ne 0 ]; then
-		echo "... failed to create configmap in kubernetes."| tee -a $LOG_FILE
+		echo "... failed to create ssl certs."| tee -a $LOG_FILE
 		exit 1
 	else
-		echo "... configmaps created in kubernetes. "| tee -a $LOG_FILE
+		echo "... ssl certs created."| tee -a $LOG_FILE
 	fi
-else
-	echo "...  configmap already exists."| tee -a $LOG_FILE
 
-fi
+}
 
-#revert quay and license keys from the Configmap file
-#hack to avoid posting license and quay key to github
-sed -i .bak "s/.dockerconfigjson: \"${quay_key}\"/.dockerconfigjson: \"\"/" $CONFIG_FILE
-sed -i .bak "s/sysdigcloud.license: \"${license_key}\"/sysdigcloud.license: \"\"/" $CONFIG_FILE
+create_tls_secret()
+{
+	#create ssl-secret in kubernetes if it doesn't exist already
+	kubectl get secret sysdigcloud-ssl-secret --namespace $NAMESPACE>> $LOG_FILE 2>&1
+	if [ $? -ne 0 ]	; then
+		kubectl create secret tls sysdigcloud-ssl-secret \
+		--cert=$SDC_HOME/etc/certs/server.crt \
+		--key=$SDC_HOME/etc/certs/server.key --namespace=sysdigcloud >> $LOG_FILE 2>&1
 
-rm ${CONFIG_FILE}.bak 
+		if [ $? -ne 0 ]; then
+			echo "... failed to create ssl secret in kubernetes."| tee -a $LOG_FILE
+			exit 1
+		else
+			echo "... ssl secret created in kubernetes. "| tee -a $LOG_FILE
+		fi
+	else
+		echo "... ssl secret sysdigcloud-ssl-secret already exists."| tee -a $LOG_FILE
+	fi	
 
-#
-#kubectl create -f etc/sdc-rbac.yaml  | tee -a $LOG_FILE
-kubectl create -f datastores/sdc-mysql-master.yaml  | tee -a $LOG_FILE
-kubectl create -f datastores/sdc-redis-master.yaml | tee -a $LOG_FILE
-kubectl create -f datastores/sdc-redis-slaves.yaml | tee -a $LOG_FILE
-kubectl create -f datastores/sdc-cassandra.yaml  | tee -a $LOG_FILE
-kubectl create -f datastores/sdc-elasticsearch.yaml | tee -a $LOG_FILE
-kubectl create -f datastores/sdc-mysql-slaves.yaml | tee -a $LOG_FILE
-#
-echo "... sleeping 60 before starting backend. " | tee -a $LOG_FILE
-sleep 60
-kubectl create  -f backend/sdc-api.yaml | tee -a $LOG_FILE
-kubectl create  -f backend/sdc-worker.yaml | tee -a $LOG_FILE
-kubectl create  -f backend/sdc-collector.yaml | tee -a $LOG_FILE
-#
-echo
-echo "... app successfully submitted to kubernetes ..." | tee -a $LOG_FILE
-echo "... monitor application by using \`watch kubectl get pods -n sysdigcloud \`" | tee -a $LOG_FILE
-echo "... wait until the sdc-api, sdc-collector and sdc-worker pods are started. " | tee -a $LOG_FILE
-echo
-echo
-echo 
-kubectl get pods -n sysdigcloud| tee -a $LOG_FILE 
+}
 
+create_configmaps()
+{
+	kubectl get configmap sysdigcloud-config --namespace $NAMESPACE>> $LOG_FILE 2>&1
+	if [ $? -ne 0 ]	; then
+		kubectl create -f $SDC_HOME/etc/config/sdc-config.yaml  --namespace $NAMESPACE
+		if [ $? -ne 0 ]; then
+			echo "... failed to create configmap in kubernetes."| tee -a $LOG_FILE
+			exit 1
+		else
+			echo "... configmaps created in kubernetes. "| tee -a $LOG_FILE
+		fi
+	else
+		echo "...  configmap already exists."| tee -a $LOG_FILE
 
+	fi
 
+}
 
+start_datastores()
+{
+	kubectl create -f $SDC_HOME/datastores/sdc-mysql-master.yaml  | tee -a $LOG_FILE
+	kubectl create -f $SDC_HOME/datastores/sdc-redis-master.yaml  | tee -a $LOG_FILE
+	kubectl create -f $SDC_HOME/datastores/sdc-redis-slaves.yaml  | tee -a $LOG_FILE
+	kubectl create -f $SDC_HOME/datastores/sdc-cassandra.yaml     | tee -a $LOG_FILE
+	kubectl create -f $SDC_HOME/datastores/sdc-elasticsearch.yaml | tee -a $LOG_FILE
+	kubectl create -f $SDC_HOME/datastores/sdc-mysql-slaves.yaml  | tee -a $LOG_FILE
+}
 
+start_backend()
+{
+	kubectl create  -f $SDC_HOME/backend/sdc-api.yaml       | tee -a $LOG_FILE
+	kubectl create  -f $SDC_HOME/backend/sdc-worker.yaml    | tee -a $LOG_FILE
+	kubectl create  -f $SDC_HOME/backend/sdc-collector.yaml | tee -a $LOG_FILE
+}
 
+print_post_install_banner()
+{
+	echo
+	echo "... app successfully submitted to kubernetes ..."                            | tee -a $LOG_FILE
+	echo "... monitor application by using \`watch kubectl get pods -n sysdigcloud \`" | tee -a $LOG_FILE
+	echo "... wait until the sdc-api, sdc-collector and sdc-worker pods are started. " | tee -a $LOG_FILE
+	echo
+	echo
+	echo 
+	kubectl get pods -n $NAMESPACE| tee -a $LOG_FILE 	
+}
 
-
+#main{}
+print_env_variables
+print_banner
+prompt_install
+create_namespace
+create_storageclasses
+create_ssl_certs
+create_tls_secret
+create_configmaps
+#start_datastores
+#echo "... about to sleep 90 before starting backend"
+#sleep 90
+#start_backend
--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-# uninstall.sh
 
-LOG_FILE=logs/uninstall/uninstall-$(date "+%Y.%m.%d-%H.%M.%S").log
-QUAY_FILE="etc/licenses/quay.uri"
-LICENSE_FILE="etc/licenses/license.uri"
-CONFIG_FILE="etc/sdc-config.yaml"
-CLOUD_PROVIDER="GKE"
-BACKEND_VERSION="800"
-FRONTEND_VERSION="0.78.1"
+# Global Variables
+NOW=$(date "+%Y.%m.%d-%H.%M.%S")
+SDC_HOME=".."
+LOG_FILE=$SDC_HOME/logs/install/install-$NOW.log
+SETTINGS_FILE="$SDC_HOME/etc/config/sdc-settings.yaml"
+NAMESPACE=$(egrep sysdigNamespace $SETTINGS_FILE |awk '{print $2}')
+BACKEND_VERSION=$(egrep sysdigBackendImage $SETTINGS_FILE | awk -F: '{print $3}')
+FRONTEND_VERSION=$(egrep sysdigFrontendImage $SETTINGS_FILE | awk -F: '{print $3}')
+K8S_CLIENT_VERSION=$(kubectl version | egrep ^Client | awk -F, '{print $3}')
+K8S_SERVER_VERSION=$(kubectl version | egrep ^Server | awk -F, '{print $3}')
+CURRENT_CONTEXT=$(kubectl config get-contexts|egrep '^\*'|awk '{print $2}')
+CURRENT_CLUSTER=$(kubectl config get-contexts|egrep '^\*'|awk '{print $3}')
+
 
 error_exit()
 {
@@ -16,91 +21,162 @@ error_exit()
 }
 
 
-echo > $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a $LOG_FILE
-echo | tee -a $LOG_FILE
-echo "This is the Sysdig Monitor on-prem Kubernetes uninstaller."| tee -a $LOG_FILE 
-echo "This installer assumes you have a running kubernetes cluster on AWS or GKE."| tee -a $LOG_FILE 
-echo "The executable 'kubectl' needs to be in your \$PATH."| tee -a $LOG_FILE 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"| tee -a $LOG_FILE 
+print_env_variables()
+{
+	printf "LOG_FILE: %s\n" $LOG_FILE
+	printf "SDC_HOME: %s\n" $SDC_HOME
+	printf "SETTINGS_FILE: %s\n" $SETTINGS_FILE
+	printf "NAMESPACE: %s\n" $NAMESPACE
+	printf "BACKEND_VERSION: %s\n" $BACKEND_VERSION
+	printf "FRONTEND_VERSION: %s\n" $FRONTEND_VERSION
+	printf "K8S_CLIENT_VERSION: %s\n" $K8S_CLIENT_VERSION
+	printf "K8S_SERVER_VERSION: %s\n" $K8S_SERVER_VERSION
+	printf "CURRENT_CONTEXT: %s\n" $CURRENT_CONTEXT
+	printf "CURRENT_CLUSTER: %s\n" $CURRENT_CLUSTER
 
-echo| tee -a $LOG_FILE 
-echo| tee -a $LOG_FILE 
+}
 
-echo "Your current kubectl client and server version are as follows:"| tee -a $LOG_FILE 
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"| tee -a $LOG_FILE 
-K8S_CLIENT_VERSION=`kubectl version | egrep ^Client | awk -F, '{print $3}'`
-K8S_SERVER_VERSION=`kubectl version | egrep ^Server | awk -F, '{print $3}'`
-echo "Client: $K8S_CLIENT_VERSION"| tee -a $LOG_FILE
-echo "Server: $K8S_SERVER_VERSION"| tee -a $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"| tee -a $LOG_FILE 
-echo| tee -a $LOG_FILE
-echo| tee -a $LOG_FILE
+print_banner()
+{
+	local cols=$(tput cols)
+	local lines=$(tput lines)
+	local numcols=$(((cols-6)/2))
+	local numlines=$((lines/2))
 
-echo "Your current Kubernetes context is:"| tee -a $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"| tee -a $LOG_FILE
-CURRENT_CONTEXT=`kubectl config get-contexts|egrep '^\*'|awk '{print $2}'`
-CURRENT_CLUSTER=`kubectl config get-contexts|egrep '^\*'|awk '{print $3}'`
-echo "Current Context: $CURRENT_CONTEXT"| tee -a $LOG_FILE
-echo "Current Cluster: $CURRENT_CLUSTER"| tee -a $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"| tee -a $LOG_FILE
-echo| tee -a $LOG_FILE
-echo| tee -a $LOG_FILE
+	clear
+	printf '\n'
+	printf '+%.0s' {1..100}
+	printf '\n'
+	#tput cup $numlines $numcols
+	printf "%s\n" "sysdigcloud on-prem uninstaller"
+	printf '\n%.0s' {1..3}
+	printf "%s\n" "This is the Sysdig Monitor on-prem Kubernetes uninstaller."
+	printf "%s\n" "This installer assumes you have a running kubernetes cluster on AWS or GKE."
+	printf "%s\n" "The executable 'kubectl' needs to be in your \$PATH with the context pointing to the right cluster."
+	printf '\n%.0s' {1..3}
+	printf "%s\n" "Your current kubectl client and server version are as follows:"
+	printf "Client: %s\n" $K8S_CLIENT_VERSION
+	printf "Server: %s\n" $K8S_SERVER_VERSION
+	printf '\n%.0s' {1..2}
+	printf "%s\n" "Your current Kubernetes context is:"
+	printf "Current Context: %s\n" $CURRENT_CONTEXT
+	printf "Current Cluster: %s\n" $CURRENT_CLUSTER
+	printf '+%.0s' {1..100}
+	printf '\n'
+}
 
-echo "Your current Kubernetes cluster on $CLOUD_PROVIDER is:" | tee -a $LOG_FILE
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a $LOG_FILE
-kubectl cluster-info
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a $LOG_FILE
-echo | tee -a $LOG_FILE
+prompt_uninstall()
+{
+	printf '\n'
+	printf '\n'
+	printf "%s\n" "Do you wish to uninstall backend version $BACKEND_VERSION of sdc-kubernetes?"| tee -a $LOG_FILE
+	select yn in "Yes" "No"; do
+	    case $yn in
+	        Yes ) break;;
+	        No ) exit 1;;
+	    esac
+	done
+	printf '\n'
 
-echo "Do you wish to uninstall backend version $BACKEND_VERSION of sdc-kubernetes along with agents with version $FRONTEND_VERSION?" | tee -a $LOG_FILE
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit 1;;
-    esac
-done
-
-kubectl delete -f etc/sdc-config.yaml  >> $LOG_FILE 2>&1
-kubectl delete -f etc/sdc-rbac.yaml  >> $LOG_FILE 2>&1
-kubectl delete secret sysdigcloud-ssl-secret -n sysdigcloud  >> $LOG_FILE 2>&1
-kubectl delete secret sysdigcloud-pull-secret -n sysdigcloud >> $LOG_FILE 2>&1
-
-kubectl delete  -f frontend/ >> $LOG_FILE 2>&1 & 
-kubectl delete  -f backend/sdc-api.yaml >> $LOG_FILE 2>&1 &
-kubectl delete  -f backend/sdc-collector.yaml  >> $LOG_FILE 2>&1 &
-kubectl delete  -f backend/sdc-worker.yaml >> $LOG_FILE 2>&1 &
-
-kubectl delete -f datastores/sdc-mysql-master.yaml  >> $LOG_FILE 2>&1 &
-kubectl delete -f datastores/sdc-mysql-slaves.yaml  >> $LOG_FILE 2>&1 &
-kubectl delete -f datastores/sdc-redis-master.yaml  >> $LOG_FILE 2>&1 &
-kubectl delete -f datastores/sdc-redis-slaves.yaml  >> $LOG_FILE 2>&1 &
-kubectl delete -f datastores/sdc-cassandra.yaml     >> $LOG_FILE 2>&1 &
-kubectl delete -f datastores/sdc-elasticsearch.yaml >> $LOG_FILE 2>&1 & 
-
-echo
-echo
-echo "... delete commands successfully submitted to kubernetes ..." | tee -a $LOG_FILE
-echo "... monitor application by using \`watch kubectl get pods -n sysdigcloud \`" | tee -a $LOG_FILE
-echo
-echo 
-kubectl get pods -n sysdigcloud| tee -a $LOG_FILE 
-
-echo
-echo
-echo
-echo "Do you wish to delete the namespace \"sysdigcloud\"?"
-echo "NB: Removing the namespace will remove all Persistent Volume Claims (PVCs) and their associated Persistent Volumes (PVs)."
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit 1;;
-    esac
-done
-kubectl delete namespace sysdigcloud 
+}
 
 
+delete_storageclasses()
+{
+	kubectl delete -f $SDC_HOME/datastores/storageclasses/ >> $LOG_FILE 2>&1
+	if [ $? -eq 0 ]; then
+		echo "... deleted storageclasses."| tee -a $LOG_FILE
+	else
+		echo "... failed to delete storageclasses."| tee -a $LOG_FILE
+
+	fi	
+}
+
+delete_ssl_certs()
+{
+	rm $SDC_HOME/etc/certs/server.key 
+	rm $SDC_HOME/etc/certs/server.crt
+	echo "...deleted ssl certs."
+	
+}
+
+delete_tls_secret()
+{
+	#create ssl-secret in kubernetes if it doesn't exist already
+	kubectl delete secret sysdigcloud-ssl-secret --namespace $NAMESPACE>> $LOG_FILE 2>&1
+	echo "... deleted ssl secret sysdigcloud-ssl-secret."| tee -a $LOG_FILE
 
 
+}
+
+delete_configmaps()
+{
+	kubectl delete -f $SDC_HOME/etc/config/sdc-config.yaml | tee -a $LOG_FILE
+	echo "... deleted configmaps."
+}
+
+stop_datastores()
+{
+	kubectl delete -f $SDC_HOME/datastores/sdc-mysql-master.yaml  | tee -a $LOG_FILE
+	kubectl delete -f $SDC_HOME/datastores/sdc-redis-master.yaml  | tee -a $LOG_FILE
+	kubectl delete -f $SDC_HOME/datastores/sdc-redis-slaves.yaml  | tee -a $LOG_FILE
+	kubectl delete -f $SDC_HOME/datastores/sdc-cassandra.yaml     | tee -a $LOG_FILE
+	kubectl delete -f $SDC_HOME/datastores/sdc-elasticsearch.yaml | tee -a $LOG_FILE
+	kubectl delete -f $SDC_HOME/datastores/sdc-mysql-slaves.yaml  | tee -a $LOG_FILE
+}
+
+stop_backend()
+{
+	kubectl delete  -f $SDC_HOME/backend/sdc-api.yaml       | tee -a $LOG_FILE
+	kubectl delete  -f $SDC_HOME/backend/sdc-worker.yaml    | tee -a $LOG_FILE
+	kubectl create  -f $SDC_HOME/backend/sdc-collector.yaml | tee -a $LOG_FILE
+}
+
+print_post_uninstall_banner()
+{
+	echo
+	echo "... app deletion order submitted to kubernetes ..."                          | tee -a $LOG_FILE
+	echo "... monitor application by using \`watch kubectl get pods -n sysdigcloud \`" | tee -a $LOG_FILE
+	echo
+	echo
+	echo 
+	kubectl get pods -n $NAMESPACE| tee -a $LOG_FILE 	
+}
+
+delete_namespace()
+{
+
+	echo "Do you wish to delete the namespace $NAMESPACE?"
+	echo "NB: Removing the namespace will remove all Persistent Volume Claims (PVCs) and their associated Persistent Volumes (PVs)."
+	select yn in "Yes" "No"; do
+	    case $yn in
+	        Yes ) break;;
+	        No ) exit 1;;
+	    esac
+	done
+	kubectl delete namespace sysdigcloud 
+	kubectl get namespace $NAMESPACE >> $LOG_FILE 2>&1
+	if [ $? -eq 0 ]	; then
+		kubectl delete namespace $NAMESPACE >> $LOG_FILE 2>&1 
+		if [ $? -eq 0 ] ; then
+			echo "... namespace $NAMESPACE deleted."| tee -a $LOG_FILE
+		else
+			echo "... failed to delete namespace $NAMESPACE."| tee -a $LOG_FILE
+			exit 1
+		fi
+	else 
+		echo "... namespace $NAMESPACE doesn't exist."| tee -a $LOG_FILE
+	fi
+}
 
 
+#main{}
+print_banner
+prompt_uninstall
+delete_storageclasses
+delete_configmaps
+delete_ssl_certs
+delete_tls_secret
+stop_datastores
+stop_backend
+delete_namespace
