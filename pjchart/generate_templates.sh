c6fc9c96753ddb84507f5b4650836b1fc0a9aa1f
Pravin Jothinathan <pravinjothinathan@gmail.com>
2019-04-24 23:29:28 -0700

    integrating collector & api yaml generations

--- pjchart/generate_templates.sh
+++ pjchart/generate_templates.sh
@@ -2,10 +2,20 @@
 
 echo "happy templating!!"
 
-echo "step1: running through helm template engine"
+echo "step1: removing exiting manifests"
+rm -rf manifests/
 
+echo "step2: creating manifest dirs"
+mkdir manifests && mkdir manifests/final
+
+echo "step1: running through helm template engine"
 helm template --values values.yaml --output-dir manifests/ .
 
-echo "step x: genrating secure configs"
+echo "step k: generating api yaml"
+kustomize build manifests/pjchart/templates/apps/overlays/api/small > manifests/final/api_config.yaml
 
-kustomize build manifests/pjchart/templates/apps/anchor/ > manifests/anchor_condig.yaml
+echo "step k: generating collector yaml"
+kustomize build manifests/pjchart/templates/apps/overlays/collector/small > manifests/final/collector_config.yaml
+
+echo "step x: genrating secure config yaml"
+kustomize build manifests/pjchart/templates/apps/anchor/ > manifests/final/secure_config.yaml
