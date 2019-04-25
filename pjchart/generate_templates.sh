2751fbf84f81c5a8532289e773dfe391fccaae62
Pravin Jothinathan <pravinjothinathan@gmail.com>
2019-04-25 15:36:18 -0700

    Adding secure components

--- pjchart/generate_templates.sh
+++ pjchart/generate_templates.sh
@@ -11,11 +11,14 @@ mkdir manifests && mkdir manifests/final
 echo "step1: running through helm template engine"
 helm template --values values.yaml --output-dir manifests/ .
 
+echo "step 5: generate postgres yaml"
+kustomize build manifests/pjchart/templates/infra/postgres/ > manifests/final/postgres.yaml
+
 echo "step k: generating api yaml"
-kustomize build manifests/pjchart/templates/apps/overlays/api/small > manifests/final/api_config.yaml
+kustomize build manifests/pjchart/templates/apps/overlays/api/small > manifests/final/api.yaml
 
 echo "step k: generating collector yaml"
-kustomize build manifests/pjchart/templates/apps/overlays/collector/small > manifests/final/collector_config.yaml
+kustomize build manifests/pjchart/templates/apps/overlays/collector/small > manifests/final/collector.yaml
 
 echo "step x: genrating secure config yaml"
-kustomize build manifests/pjchart/templates/apps/anchor/ > manifests/final/secure_config.yaml
+kustomize build manifests/pjchart/templates/apps/secure/ > manifests/final/secure.yaml
