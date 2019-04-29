7beaa04d8641f49520ead6ae0552b267d194ea9d
Pravin Jothinathan <pravinjothinathan@gmail.com>
2019-04-29 16:34:06 -0700

    fixing step numbers

--- pjchart/generate_templates.sh
+++ pjchart/generate_templates.sh
@@ -8,6 +8,7 @@ rm -rf manifests/
 echo "step2: creating manifest dirs"
 mkdir manifests && mkdir manifests/final
 
+echo "step3: creating secret file - if it does not exist"
 SECRET_FILE=secrets.yaml
 if [ -f "$SECRET_FILE" ]; then
     echo "$SECRET_FILE exists"
@@ -16,38 +17,38 @@ else
     helm template -x templates/secrets.yaml secrets > secrets.yaml
 fi
 
-echo "step1: running through helm template engine"
+echo "step4: running through helm template engine"
 helm template -f values.yaml -f secrets.yaml --output-dir manifests/ .
 
-echo "step2: generate commong files"
+echo "step5: generate commong files"
 kustomize build manifests/pjchart/templates/overlays/common-config/small/ > manifests/final/common-config.yaml
 
-echo "step3:  Generating data-stores"
-echo "step3a: data-stores cassandra"
+echo "step6:  Generating data-stores"
+echo "step6a: data-stores cassandra"
 echo "---" >> manifests/final/infra.yaml
 kustomize build manifests//pjchart/templates/data-stores/overlays/cassandra/small/    >> manifests/final/infra.yaml
-echo "step3b: data-stores elasticsearch"
+echo "step6b: data-stores elasticsearch"
 echo "---" >> manifests/final/infra.yaml
 kustomize build manifests/pjchart/templates/data-stores/overlays/elasticsearch/small/ >> manifests/final/infra.yaml
-echo "step3c: data-stores mysql"
+echo "step6c: data-stores mysql"
 echo "---" >> manifests/final/infra.yaml
 kustomize build manifests//pjchart/templates/data-stores/overlays/mysql-single/small/ >> manifests/final/infra.yaml
-echo "step3d: data-stores postgres"
+echo "step6d: data-stores postgres"
 echo "---" >> manifests/final/infra.yaml
 kustomize build manifests//pjchart/templates/data-stores/overlays/postgres/small/     >> manifests/final/infra.yaml
-echo "step3e: data-stores redis"
+echo "step6e: data-stores redis"
 echo "---" >> manifests/final/infra.yaml
 kustomize build manifests//pjchart/templates/data-stores/redis-single/                >> manifests/final/infra.yaml
 
-echo "step 4: Generating monitor"
-echo "step 4a: generate monitor-api yamls"
+echo "step 7: Generating monitor"
+echo "step 7a: generate monitor-api yamls"
 kustomize build manifests//pjchart/templates/sysdig-cloud/overlays/api/small/              >  manifests/final/api.yaml
 
-echo "step 4b: generate monitor-collectorworker yamls"
+echo "step 7b: generate monitor-collectorworker yamls"
 kustomize build manifests//pjchart/templates/sysdig-cloud/overlays/collector-worker/small/ >  manifests/final/collector-worker.yaml
 
-echo "step 5: genrating secure yaml"
+echo "step 8: genrating secure yaml"
 kustomize build manifests/pjchart/templates/sysdig-cloud/secure/                           > manifests/final/secure.yaml
 
-echo "step 6: generate ingress yaml"
+echo "step 9: generate ingress yaml"
 kustomize build manifests/pjchart/templates/sysdig-cloud/ingress_controller/               > manifests/final/ingress.yaml
