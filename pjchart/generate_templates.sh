6dc6d68badc722f7646e7d83c0c2649e94f307a3
Pravin Jothinathan <pravinjothinathan@gmail.com>
2019-05-10 12:47:16 -0700

    kustomize overlays for redis-ha

--- pjchart/generate_templates.sh
+++ pjchart/generate_templates.sh
@@ -47,10 +47,10 @@ kustomize build manifests/pjchart/templates/sysdig-cloud/ingress_controller/    
 echo "step7:  Generating data-stores"
 echo "step7a: data-stores cassandra"
 echo "---" >>$GENERATED_DIR/infra.yaml
-kustomize build manifests//pjchart/templates/data-stores/overlays/cassandra/$size/    >> $GENERATED_DIR/infra.yaml
+kustomize build manifests//pjchart/templates/data-stores/overlays/cassandra/$size/      >> $GENERATED_DIR/infra.yaml
 echo "step7b: data-stores elasticsearch"
 echo "---" >>$GENERATED_DIR/infra.yaml
-kustomize build manifests/pjchart/templates/data-stores/overlays/elasticsearch/$size/ >> $GENERATED_DIR/infra.yaml
+kustomize build manifests/pjchart/templates/data-stores/overlays/elasticsearch/$size/   >> $GENERATED_DIR/infra.yaml
 if [ $size = "small" ]; then
   echo "step7c: data-stores mysql-single small"
   echo "---" >>$GENERATED_DIR/infra.yaml
@@ -63,13 +63,19 @@ fi
 if [ $mode = "monitor+secure" ]; then
   echo "step7d: data-stores postgres"
   echo "---" >>$GENERATED_DIR/infra.yaml
-  kustomize build manifests//pjchart/templates/data-stores/overlays/postgres/small/   >> $GENERATED_DIR/infra.yaml
+  kustomize build manifests//pjchart/templates/data-stores/overlays/postgres/small/     >> $GENERATED_DIR/infra.yaml
 else
   echo "skipping step7d: data-stores postgres - needed only for secure"
 fi
-echo "step7e: data-stores redis"
-echo "---" >>$GENERATED_DIR/infra.yaml
-kustomize build manifests//pjchart/templates/data-stores/redis-single/                >> $GENERATED_DIR/infra.yaml
+if [ $size = "small" ]; then
+  echo "step7e: data-stores redis-single small"
+  echo "---" >>$GENERATED_DIR/infra.yaml
+  kustomize build manifests//pjchart/templates/data-stores/redis-single/                   >> $GENERATED_DIR/infra.yaml
+else
+  echo "step7e: data-stores redis-ha $size"
+  echo "---" >>$GENERATED_DIR/infra.yaml
+  kustomize build manifests//pjchart/templates/data-stores/overlays/redis-stateful/$size   >> $GENERATED_DIR/infra.yaml
+fi
 
 echo "step 8: Generating monitor"
 echo "step 8a: generate monitor-api yamls"
