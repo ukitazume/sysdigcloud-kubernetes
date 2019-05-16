546f06a9bde45dd27c41da1182b63a22b39359da
Pravin Jothinathan <pravinjothinathan@gmail.com>
2019-05-16 16:35:50 -0700

    Adding deply script for secure

---
+++ pjchart/secure.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+#Important framework functions.
+. framework.sh
+
+broadcast 'g' "Deploying AnchoreCore"
+ka manifests/generated/anchore-core.yaml
+wait_for_pods 10
+
+broadcast 'g' "Deploying AncoreWorker"
+ka manifests/generated/anchore-worker.yaml
+wait_for_pods 10
+
+broadcast 'g' "Deploying Scanning"
+ka manifests/generated/scanning.yaml
+wait_for_pods 10
