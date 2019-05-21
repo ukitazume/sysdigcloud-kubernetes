51e143a57128b3eb29b18f9f9aced297437904a2
Pravin Jothinathan <pravinjothinathan@gmail.com>
2019-05-21 15:24:47 -0700

    using absolute path + fixing delete secrets & certs

--- configurator/sysdig-chart/generate_templates.sh
+++ configurator/sysdig-chart/generate_templates.sh
@@ -28,24 +28,29 @@ GENERATED_DIR=$MANIFESTS/generated
 mkdir $GENERATED_DIR
 
 echo "step3: creating secret file - if it does not exist"
+TEMPLATE_DIR=/sysdig-chart
 SECRET_FILE=secrets-values.yaml
-GENERATED_SECRET_FILE=$GENERATED_DIR/secrets-values.yaml
+GENERATED_SECRET_FILE=$MANIFESTS/secrets-values.yaml
 if [ -f "$GENERATED_SECRET_FILE" ]; then
     echo "$SECRET_FILE exists"
 else
     echo "Secret file does not exist. Creating Secretfile"
-    helm template -x templates/$SECRET_FILE secret-generator > $GENERATED_SECRET_FILE
+    helm template -x templates/$SECRET_FILE $TEMPLATE_DIR/secret-generator > $GENERATED_SECRET_FILE
 fi
 
 echo "step4: running through helm template engine"
-helm template -f values.yaml -f $GENERATED_SECRET_FILE --output-dir $MANIFESTS .
+helm template -f $TEMPLATE_DIR/values.yaml -f $GENERATED_SECRET_FILE --output-dir $MANIFESTS $TEMPLATE_DIR
 
-TEMPLATE_BASE=$MANIFESTS/sysdig-chart/templates/
-GENERATE_CERTIFICATE=$(cat values.yaml | yq .sysdig.certificate.generate)
-GENERATED_CRT=$GENERATED_DIR/certs/server.crt
-GENERATED_KEY=$GENERATED_DIR/certs/server.key
-DNS_NAME=$(cat values.yaml | yq .sysdig.dnsName)
-mkdir $TEMPLATE_BASE/common-config/certs && mkdir $GENERATED_DIR/certs
+MANIFESTS_TEMPLATE_BASE=$MANIFESTS/sysdig-chart/templates/
+GENERATE_CERTIFICATE=$(cat $TEMPLATE_DIR/values.yaml | yq .sysdig.certificate.generate)
+GENERATED_CRT=$MANIFESTS/certs/server.crt
+GENERATED_KEY=$MANIFESTS/certs/server.key
+DNS_NAME=$(cat $TEMPLATE_DIR/values.yaml | yq .sysdig.dnsName)
+mkdir $MANIFESTS_TEMPLATE_BASE/common-config/certs
+if [ ! -d mkdir $MANIFESTS/certs]; then
+  echo "Making certs manifests dir"
+  mkdir $MANIFESTS/certs
+fi
 if [ "$GENERATE_CERTIFICATE" = true ]; then
   if [[ -f $GENERATED_KEY && -f $GENERATED_CRT ]]; then
     echo "Certificates are present. Copying the existing certs"
@@ -53,14 +58,14 @@ if [ "$GENERATE_CERTIFICATE" = true ]; then
     echo "Generating new certificate"
     openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj "/C=US/ST=CA/L=SanFrancisco/O=ICT/CN=$DNS_NAME" -keyout $GENERATED_KEY -out $GENERATED_CRT
   fi
-  cp $GENERATED_KEY $GENERATED_CRT $TEMPLATE_BASE/common-config/certs/
+  cp $GENERATED_KEY $GENERATED_CRT $MANIFESTS_TEMPLATE_BASE/common-config/certs/
 else
-  CRT_FILE=$(cat values.yaml | yq .sysdig.certificate.crt | tr -d '"')
-  KEY_FILE=$(cat values.yaml | yq .sysdig.certificate.key | tr -d '"')
+  CRT_FILE=$(cat $TEMPLATE_DIR/values.yaml | yq .sysdig.certificate.crt | tr -d '"')
+  KEY_FILE=$(cat $TEMPLATE_DIR/values.yaml | yq .sysdig.certificate.key | tr -d '"')
   echo "Using provided certificates at crt:$CRT_FILE key:$KEY_FILE"
   if [[ -f $CRT_FILE && -f $KEY_FILE ]]; then
-    cp $CRT_FILE $TEMPLATE_BASE/common-config/certs/server.crt
-    cp $KEY_FILE $TEMPLATE_BASE/common-config/certs/server.key
+    cp $CRT_FILE $MANIFESTS_TEMPLATE_BASE/common-config/certs/server.crt
+    cp $KEY_FILE $MANIFESTS_TEMPLATE_BASE/common-config/certs/server.key
   else
     echo "Cannot find certificate files. Exiting"
     exit 2
@@ -68,51 +73,51 @@ else
 fi
 
 echo "step5: generate commong files"
-kustomize build $TEMPLATE_BASE/overlays/common-config/small                  > $GENERATED_DIR/common-config.yaml
+kustomize build $MANIFESTS_TEMPLATE_BASE/overlays/common-config/small                  > $GENERATED_DIR/common-config.yaml
 
 echo "step 6: generate ingress yaml"
-kustomize build $TEMPLATE_BASE/sysdig-cloud/ingress_controller               > $GENERATED_DIR/ingress.yaml
+kustomize build $MANIFESTS_TEMPLATE_BASE/sysdig-cloud/ingress_controller               > $GENERATED_DIR/ingress.yaml
 
 echo "step7:  Generating data-stores"
 echo "step7a: data-stores cassandra"
 echo "---" >>$GENERATED_DIR/infra.yaml
-kustomize build $TEMPLATE_BASE/data-stores/overlays/cassandra/$size          >> $GENERATED_DIR/infra.yaml
+kustomize build $MANIFESTS_TEMPLATE_BASE/data-stores/overlays/cassandra/$size          >> $GENERATED_DIR/infra.yaml
 echo "step7b: data-stores elasticsearch"
 echo "---" >>$GENERATED_DIR/infra.yaml
-kustomize build $TEMPLATE_BASE/data-stores/overlays/elasticsearch/$size      >> $GENERATED_DIR/infra.yaml
+kustomize build $MANIFESTS_TEMPLATE_BASE/data-stores/overlays/elasticsearch/$size      >> $GENERATED_DIR/infra.yaml
 echo "step7c: data-stores mysql $size"
 echo "---" >>$GENERATED_DIR/infra.yaml
-kustomize build $TEMPLATE_BASE/data-stores/overlays/mysql/$size              >> $GENERATED_DIR/infra.yaml
+kustomize build $MANIFESTS_TEMPLATE_BASE/data-stores/overlays/mysql/$size              >> $GENERATED_DIR/infra.yaml
 if [ $mode = "monitor+secure" ]; then
   echo "step7d: data-stores postgres"
   echo "---" >>$GENERATED_DIR/infra.yaml
-  kustomize build $TEMPLATE_BASE/data-stores/overlays/postgres/$size         >> $GENERATED_DIR/infra.yaml
+  kustomize build $MANIFESTS_TEMPLATE_BASE/data-stores/overlays/postgres/$size         >> $GENERATED_DIR/infra.yaml
 else
   echo "skipping step7d: data-stores postgres - needed only for secure"
 fi
 if [ $size = "small" ]; then
   echo "step7e: data-stores redis-single small"
   echo "---" >>$GENERATED_DIR/infra.yaml
-  kustomize build $TEMPLATE_BASE/data-stores/redis-single                    >> $GENERATED_DIR/infra.yaml
+  kustomize build $MANIFESTS_TEMPLATE_BASE/data-stores/redis-single                    >> $GENERATED_DIR/infra.yaml
 else
   echo "step7e: data-stores redis-ha $size"
   echo "---" >>$GENERATED_DIR/infra.yaml
-  kustomize build $TEMPLATE_BASE/data-stores/overlays/redis-stateful/$size   >> $GENERATED_DIR/infra.yaml
+  kustomize build $MANIFESTS_TEMPLATE_BASE/data-stores/overlays/redis-stateful/$size   >> $GENERATED_DIR/infra.yaml
 fi
 
 echo "step 8: Generating monitor"
 echo "step 8a: generate monitor-api yamls"
-kustomize build $TEMPLATE_BASE/sysdig-cloud/overlays/api/$size               > $GENERATED_DIR/api.yaml
+kustomize build $MANIFESTS_TEMPLATE_BASE/sysdig-cloud/overlays/api/$size               > $GENERATED_DIR/api.yaml
 
 echo "step 8b: generate monitor-collectorworker yamls"
-kustomize build $TEMPLATE_BASE/sysdig-cloud/overlays/collector-worker/$size  > $GENERATED_DIR/collector-worker.yaml
+kustomize build $MANIFESTS_TEMPLATE_BASE/sysdig-cloud/overlays/collector-worker/$size  > $GENERATED_DIR/collector-worker.yaml
 
 if [ $mode = "monitor+secure" ]; then
   echo "step 9a: generating secure-scanning yaml"
-  kustomize build $TEMPLATE_BASE/sysdig-cloud/overlays/secure/scanning/$size       > $GENERATED_DIR/scanning.yaml
+  kustomize build $MANIFESTS_TEMPLATE_BASE/sysdig-cloud/overlays/secure/scanning/$size       > $GENERATED_DIR/scanning.yaml
   echo "step 9b: generating secure-anchore yaml"
-  kustomize build $TEMPLATE_BASE/sysdig-cloud/overlays/secure/anchore/$size        > $GENERATED_DIR/anchore-core.yaml
-  kustomize build $TEMPLATE_BASE/sysdig-cloud/overlays/secure/anchore/worker/$size > $GENERATED_DIR/anchore-worker.yaml
+  kustomize build $MANIFESTS_TEMPLATE_BASE/sysdig-cloud/overlays/secure/anchore/$size        > $GENERATED_DIR/anchore-core.yaml
+  kustomize build $MANIFESTS_TEMPLATE_BASE/sysdig-cloud/overlays/secure/anchore/worker/$size > $GENERATED_DIR/anchore-worker.yaml
 else
   echo "skipping step 9: genrating secure yaml - needed only for secure"
 fi
