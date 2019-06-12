a59cdeeb6bef2a60531acf79480554b10f95c0d2
Pravin Jothinathan <pravinjothinathan@gmail.com>
2019-06-11 17:26:50 -0700

    Creates openshift routes for api and controller

--- configurator/sysdig-chart/openshift.sh
+++ configurator/sysdig-chart/openshift.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
-set -euo pipefail
+set -uo pipefail
 #set variables
 alias kubectl="oc-kubectl"
 OPENSHIFT_URL=$(cat /sysdig-chart/values.yaml | yq .sysdig.openshiftUrl | tr -d '"')
@@ -27,3 +27,29 @@ fi
 #create permission for the project
 oc adm policy add-scc-to-user anyuid -n ${NAMESPACE} -z default
 oc adm policy add-scc-to-user privileged -n ${NAMESPACE} -z default
+
+DNS_NAME=$(cat /sysdig-chart/values.yaml | yq .sysdig.dnsName | tr -d '"')
+
+STATUS_API=$(oc get route sysdigcloud-api)
+if $?; then
+  echo "Route sysdigcloud-api exists."
+else
+  echo "Creating sysdigcloud-api route"
+  oc create route edge sysdigcloud-api \
+    --service=sysdigcloud-api \
+    --cert=/manifests/certs/server.crt \
+    --key=/manifests/certs/server.key \
+    --port=8080 \
+    --hostname=${DNS_NAME}
+fi
+
+STATUS_COLLECTOR=$(oc get route sysdigcloud-collector)
+if $?; then
+  echo "Route sysdigcloud-collector exists."
+else
+  echo "Creating route sysdigcloud-collector"
+  oc create route passthrough sysdigcloud-collector \
+    --service sysdigcloud-collector \
+    --port=6443 \
+    --hostname=collector-${DNS_NAME}
+fi
